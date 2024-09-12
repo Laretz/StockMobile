5e729004a0ed00054d15438468bbd9106616cf64
Renato Ramon <renatophoenix123@gmail.com>
2024-09-12 17:53:47 -0300

    feat: add sign up viewmodel

--- StockMobile/StockMobile/ViewModels/SignUpViewModel.cs
+++ StockMobile/StockMobile/ViewModels/SignUpViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CommunityToolkit.Maui.Alerts;
+using StockMobile.Models.Request;
+using StockMobile.Repositories.SignUp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,44 @@ using System.Threading.Tasks;
 
 namespace StockMobile.ViewModels
 {
-    internal class SignUpViewModel
+    public partial class SignUpViewModel : BaseViewModel
     {
+        private readonly ISignUpRepository _signUpRepository;
+        public SignUpViewModel(ISignUpRepository signUpRepository)
+        {
+            _signUpRepository = signUpRepository;
+        }
+        [ObservableProperty]
+        string name;
+
+        [ObservableProperty]
+        string password;
+
+        [ObservableProperty]
+        string email;
+
+        [RelayCommand]
+        public async Task Signup()
+        { 
+            var request = new SignupRequest(Name, Email, Password);
+            // validações
+
+            var result = await _signUpRepository.CreateAsync(request);
+
+            if (result)
+            {
+                var toast = Toast.Make("Usuario cadastrado com sucesso!", CommunityToolkit.Maui.Core.ToastDuration.Long);
+                await toast.Show();
+
+                await Shell.Current.GoToAsync("..");
+            }
+            else
+            {
+                var toast = Toast.Make("Erro ao cadastrar o usuario", CommunityToolkit.Maui.Core.ToastDuration.Long);
+                await toast.Show();
+
+                
+            }
+        }
     }
 }
